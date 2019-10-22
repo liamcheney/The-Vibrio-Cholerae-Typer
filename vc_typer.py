2418e8f6298efde4482856e73611eb4b83f0ede4
liamcheney <liam.cheney1@gmail.com>
2019-10-22 14:19:02 +1100

    test

--- vc_typer.py
+++ vc_typer.py
@@ -3,187 +3,104 @@ import glob
 import sys
 from os import path
 from Bio.Blast.Applications import NcbiblastnCommandline
-import multiprocessing
 import datetime
-import progressbar
-from operator import itemgetter
 from time import sleep as sl
 from Bio import SeqIO
 
-#smaller functions
-def check_databases_input(args):
-    database_okay = True
-    accepted_databases = ['All','sero','ctxB','tcpA','rstR','sxt', 'ICE_check', 'seventh_check', 'species_check']
-    in_split = args.databases.split(',')
-    for i in in_split:
-        if i not in accepted_databases:
-            database_okay = False
-    return database_okay
-def input_allele_lengths(set_wd, args):
+def run_main(set_wd, args):
+    ##set blast_path, database list
+    blast_db_path = set_blast_path(set_wd)
+    databases = databases_list(args)
 
-    gene_size_dict = {}
-    for record in SeqIO.parse(set_wd + "/blast_db/alleles.fasta", "fasta"):
-        gene = record.id
-        length = len(str(record.seq))
-        gene_size_dict[gene] = length
+    ##output during analysis
+    print("Blasting database alleles against genomes.")
+    all = len([x for x in glob.iglob(args.strains_directory + '/*.f*')])
+    genome_count = 1
 
-    return gene_size_dict
-def databases_list(args):
+    ##blast results
+    blast_result_dict = {}
+    for strain in glob.iglob(args.strains_directory + '/*.f*'):
+        accession = strain.split('/')[-1].split('.')[0]
 
-    if args.databases == 'All':
-        database_list = ['sero', 'ctxB', 'tcpA', 'rstR', 'sxt', 'ICE_check', 'seventh_check',
-                              'species_check']
-        return database_list
+        # get blast results
+        blast_result = blast_input_against_db(strain, blast_db_path, args)
+        blast_result = blast_result_filtering(blast_result, databases, set_wd, args)
+        blast_result_dict[accession] = blast_result
 
-    else:
-        input_database_list = args.databases.split(',')
-        database_list = []
+        print_list = []
+        for i in blast_result:
+            print_list.append(blast_result[i].split(',')[1])
+        print(accession, *print_list, sep='\t')
 
-        for el in input_database_list:
-            database_list.append(el)
+    ##writing out
+    write_out(blast_result_dict, args)
 
-        return database_list
-def progess_use(args):
-    number_of_genomes = len(list(glob.iglob(args.strains_directory + '/*.f*'))) + 1
-    bar = progressbar.ProgressBar(maxval=number_of_genomes,
-                                  widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-
-    return bar
-def query_length_gen(col):
-
-    if int(col[9]) > int(col[8]):
-        query_length = int(col[9]) - int(col[8]) + 1
-    else:
-        query_length = int(col[8]) - int(col[9]) + 1
-
-    return query_length
-def parseargs(set_wd):
-
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-db", "--databases", default='All',
-                        help="Databases to process. Options ctxB, tcpA, rstR, sxt, \
-                             ICE_check, seventh_check, species_check, \
-                             For combinations use comma seperated. Eg. ctxB,tcpA \
-                             Default is all databases.")
-    parser.add_argument("-dir", "--strains_directory", required=True,
-                        help="A directory of strains to analyse.")
-    parser.add_argument("-r", "--return_all_blast", action='store_true',
-                        help="Return all blast hits")
-    parser.add_argument("-o", "--output_folder", default=set_wd + "/output/",
-                        help="Output folder to save if not specified.")
-    parser.add_argument("-f", "--overwrite", action='store_true',
-                        help="Overwrite previous results output file.")
-    parser.add_argument("-cov", "--coverage", default=95,
-                        help="Minimum percentage of blast query length.")
-    parser.add_argument("-len", "--length", default=95,
-                        help="Minmum percetange gene can be missing nucleotides.")
-    parser.add_argument("-t", "--threads", default=1,
-                        help="Threads for computing.")
-
-
-
-    args = parser.parse_args()
-
-    return args
-
-#blast searching
-def blast_search(set_wd, args):
+# blast searching
+def set_blast_path(set_wd):
 
     print("Analaying strains.")
     current_blast_db_path = set_wd + "/blast_db/alleles"
-    results_dict = run_genomes(current_blast_db_path, args)
 
-    return results_dict
-def run_genomes(current_blast_db_path, args):
-    print("Blasting database alleles against genomes.")
-    bar = progess_use(args)
-    bar.start()
-    genome_count = 0
-    blast_results_dict = {}
-    for strain in glob.iglob(args.strains_directory + '/*.f*'):
-        bar.update(genome_count)
-        genome_count = genome_count + 1
-        accession = strain.split('/')[-1].split('.')[0]
-        blast_result = blast_input_against_db(strain, current_blast_db_path, args)
-        blast_results_dict[accession] = blast_result
-    bar.finish()
+    return current_blast_db_path
+def blast_input_against_db(strain, blast_db_path, args):
 
-    results_dict = blast_result_filtering(blast_results_dict, args)
-
-    return results_dict
-def blast_input_against_db(strain, current_blast_db_path, args):
     #blast query database against genome
-
-    blast_string = NcbiblastnCommandline(task="blastn", query=strain, db=current_blast_db_path, outfmt=6, num_threads=args.threads)
+    blast_string = NcbiblastnCommandline(task="blastn", query=strain, db=blast_db_path, outfmt=6, num_threads=args.threads)
     out, err = blast_string()
+    out = out.splitlines()
     return out
-def blast_result_filtering(blast_results_dict, args):
+def blast_result_filtering(blast_results_dict, databases, set_wd, args):
 
-    print("Processing blast outputs.")
     # read in file of allele lengths
-    set_wd = path.dirname(path.abspath(__file__))
     gene_size_dict = input_allele_lengths(set_wd, args)
 
-    #store the blast results and create a dict for all results
-    returning_results = {}
+    #seperate blast results based on database
+    sep_blast_results = seperate_blast_results(blast_results_dict, databases)
 
-    #get list of databases
-    databases = databases_list(args)
+    #select the hits from each databaes
+    result_dict = selecting_hits(sep_blast_results, gene_size_dict, args)
 
-    #go over each selected databases
-    for item in databases:
-            returning_results[item] = remaining_filter_results(blast_results_dict, item, gene_size_dict,args)
-
-    return returning_results
+    return result_dict
 
 #handling alleles
-def remaining_filter_results(blast_results_dict, item, gene_size_dict,args):
+def seperate_blast_results(blast_results_dict, databases):
+    result_dict = {}
+    for item in databases:
+        result_dict[item] = []
 
-    return_dict = {}
-    #go over each strain result for the database
-    for strain in blast_results_dict.keys():
-        #remove non relevant results and organise list
-        format_blast_list = remaining_format_blast_output(blast_results_dict[strain], item)
+    for key in result_dict:
+        for line in blast_results_dict:
+            col = line.split()
+            allele = col[1]
+            if key in allele:
+                result_dict[key].append(line)
 
-        #check the blast results
-        return_dict[strain] = remaining_blast_filter(format_blast_list, item, gene_size_dict,args)
+    return result_dict
+def selecting_hits(sep_blast_results, gene_size_dict, args):
 
-    return return_dict
-def remaining_format_blast_output(results_list, item):
+    result_dict = {}
+    for key, value in sep_blast_results.items():
+        format_blast_list = [x.split('\t') for x in value]
+        format_blast_list.sort(key=lambda x: (float(x[2]),int(x[3])), reverse=True)
 
-    #split the blast result
-    blast_hits = results_list.split('\n')[:-1] #remove last new line char
+        for element in format_blast_list:
+            allele_length = gene_size_dict[element[1]]
+            min_return_allele_length = int(args.length / 100 * allele_length)
+            min_return_coverage = int(args.coverage / 100 * allele_length)
 
-    keep_list = []
-    for number in range(0, len(blast_hits), 1):
-        if item in blast_hits[number].split('\t')[1]:
-            keep_list.append(blast_hits[number])
+            if args.return_all_blast:
+                if (min_return_allele_length <= int(element[3])) and (min_return_coverage <= int(element[3])):
+                    keep = ','.join(element)
+                    result_dict[key] = keep
 
-    return keep_list
-def remaining_blast_filter(format_blast_list, item, gene_size_dict, args):
+            if not args.return_all_blast:
+                if (min_return_allele_length <= int(element[3])) and (min_return_coverage <= int(element[3])):
+                    top_hit = format_blast_list[0]
+                    keep = ','.join(top_hit)
+                    result_dict[key] = keep
+                    break
 
-    result_list = []
-    format_blast_list = [x.split('\t') for x in format_blast_list]
-    format_blast_list.sort(key=lambda x: (float(x[2]),int(x[3])), reverse=True)
-
-    for element in format_blast_list:
-        allele_length = gene_size_dict[element[1]]
-        min_return_allele_length = int(args.length / 100 * allele_length)
-        min_return_coverage = int(args.coverage / 100 * allele_length)
-
-        if args.return_all_blast:
-            if (min_return_allele_length <= int(element[3])) and (min_return_coverage <= int(element[3])):
-                keep = ','.join(element)
-                result_list.append(keep)
-
-        if not args.return_all_blast:
-            if (min_return_allele_length <= int(element[3])) and (min_return_coverage <= int(element[3])):
-                top_hit = format_blast_list[0]
-                keep = ','.join(top_hit)
-                result_list.append(keep)
-                break
-
-    return result_list
+    return result_dict
 
 ##writing out
 def write_out(results_dict, args):
@@ -219,23 +136,87 @@ def write_out_iterator(results_dict, out, args):
         for strain in results_dict[key]:
             out.write(strain + ',')
             for item in results_dict[key][strain]:
-                out.write(item + ',')
+                out.write(item)
             out.write('\n')
 
         out.write('\n')
 def simple_write_out_iterator(results_dict, out, args):
-    # column names
-    headers_list = ["Accession", "sseqid"]
-
     # writing out all outputs blast results
-    for key in results_dict:
-        for col_head in headers_list:
-            out.write(col_head + ',')
+    for strain in results_dict:
+        out.write(strain + ',')
+        for gene in results_dict[strain]:
+            out.write(results_dict[strain][gene].split(',')[1] + ',')
         out.write('\n')
 
-        for strain in results_dict[key]:
-            out.write(strain + ',' + results_dict[key][strain][0].split(',')[1] + '\n')
-        out.write('\n')
+#smaller functions
+def check_databases_input(args):
+    database_okay = True
+    accepted_databases = ['All','sero','ctxB','tcpA','rstR','sxt', 'ICE_check', 'seventh_check', 'species_check']
+    in_split = args.databases.split(',')
+    for i in in_split:
+        if i not in accepted_databases:
+            database_okay = False
+    return database_okay
+def input_allele_lengths(set_wd, args):
+
+    gene_size_dict = {}
+    for record in SeqIO.parse(set_wd + "/blast_db/alleles.fasta", "fasta"):
+        gene = record.id
+        length = len(str(record.seq))
+        gene_size_dict[gene] = length
+
+    return gene_size_dict
+def databases_list(args):
+
+    if args.databases == 'All':
+        database_list = ['sero', 'ctxB', 'tcpA', 'rstR', 'sxt', 'ICE_check', 'seventh_check',
+                              'species_check']
+        return database_list
+
+    else:
+        input_database_list = args.databases.split(',')
+        database_list = []
+
+        for el in input_database_list:
+            database_list.append(el)
+
+        return database_list
+def query_length_gen(col):
+
+    if int(col[9]) > int(col[8]):
+        query_length = int(col[9]) - int(col[8]) + 1
+    else:
+        query_length = int(col[8]) - int(col[9]) + 1
+
+    return query_length
+def parseargs(set_wd):
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-db", "--databases", default='All',
+                        help="Databases to process. Options ctxB, tcpA, rstR, sxt, \
+                             ICE_check, seventh_check, species_check, \
+                             For combinations use comma seperated. Eg. ctxB,tcpA \
+                             Default is all databases.")
+    parser.add_argument("-dir", "--strains_directory", required=True,
+                        help="A directory of strains to analyse.")
+    parser.add_argument("-r", "--return_all_blast", action='store_true',
+                        help="Return all blast hits")
+    parser.add_argument("-o", "--output_folder",
+                        help="Output folder to save if not specified.")
+    parser.add_argument("-f", "--overwrite", action='store_true',
+                        help="Overwrite previous results output file.")
+    parser.add_argument("-cov", "--coverage", default=95, type=int,
+                        help="Minimum percentage of blast query length.")
+    parser.add_argument("-len", "--length", default=95, type=int,
+                        help="Minmum percetange gene can be missing nucleotides.")
+    parser.add_argument("-t", "--threads", default=1,
+                        help="Threads for computing.")
+
+
+
+    args = parser.parse_args()
+
+    return args
 
 def main():
 
@@ -243,8 +224,7 @@ def main():
     args = parseargs(set_wd)
     database_input = check_databases_input(args)
     if database_input:
-        results_dict = blast_search(set_wd, args)
-        write_out(results_dict, args)
+        run_main(set_wd, args)
     else:
         print('One of following database(s) is incorrect : ' + str(args.databases))
         sys.exit()
